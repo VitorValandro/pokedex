3c8f91e501d9dc48bced87f38ca36e8618764750
Vitor Valandro <vitormateusd@gmail.com>
2022-02-02 14:26:49 -0300

    App development boilerplate

---
+++ src/Landing.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <>
+      <Header />
+      <div className="landing-container">
+
+      </div>
+    </>
+  );
+}
+
+export default Landing;
